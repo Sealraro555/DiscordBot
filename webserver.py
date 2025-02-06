2ade15b73f57d048952e2f6cb7309f76f12a9f92
Sealraro555 <sealraro555@gmail.com>
2025-02-06 17:51:09 -0500

    webserver.py

--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os  # ← Añade esta importación
 
 app = Flask('')
 
@@ -8,8 +9,8 @@ def index():
     return 'Hello from Flask!'
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))  # Usa la variable PORT
 
 def keep_alive():
     server = Thread(target=run)
-    server.start
+    server.start()
